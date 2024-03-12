58d9e03480ae115e68b0a4e2674a698a994f2a59
AdityaDwiputraSubroto <adityadwiputras23@gmail.com>
2024-03-12 14:30:13 +0700

    fix overflow HomeScreen

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -26,75 +26,77 @@ class _HomeScreenState extends State<HomeScreen> {
               IconThemeData(color: ColorsCalc.hexToColor(ColorsCalc.white)),
           backgroundColor: ColorsCalc.hexToColor(ColorsCalc.green),
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 120,
-              ),
-              Text(
-                'Choose Menu',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              ButtonCalc(
-                  buttonText: 'Calculator',
-                  width: 300,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return CalculatorScreen();
-                    }));
-                  }),
-              SizedBox(
-                height: 30,
-              ),
-              ButtonCalc(
-                  buttonText: 'Ganjil / Genap',
-                  width: 300,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return GanjilGenapScreen();
-                    }));
-                  }),
-              SizedBox(
-                height: 30,
-              ),
-              ButtonCalc(
-                  buttonText: 'Data Kelompok',
-                  width: 300,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return DataKelompokScreen();
-                    }));
-                  }),
-              SizedBox(
-                height: 30,
-              ),
-              ButtonCalc(
-                  buttonText: 'Logout',
-                  buttonColor: ColorsCalc.orange,
-                  width: 300,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80)),
-                  onPressed: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Loginpage();
-                    }));
-                  })
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Choose Menu',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                ButtonCalc(
+                    buttonText: 'Calculator',
+                    width: 300,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return CalculatorScreen();
+                      }));
+                    }),
+                SizedBox(
+                  height: 30,
+                ),
+                ButtonCalc(
+                    buttonText: 'Ganjil / Genap',
+                    width: 300,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return GanjilGenapScreen();
+                      }));
+                    }),
+                SizedBox(
+                  height: 30,
+                ),
+                ButtonCalc(
+                    buttonText: 'Data Kelompok',
+                    width: 300,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return DataKelompokScreen();
+                      }));
+                    }),
+                SizedBox(
+                  height: 30,
+                ),
+                ButtonCalc(
+                    buttonText: 'Logout',
+                    buttonColor: ColorsCalc.orange,
+                    width: 300,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80)),
+                    onPressed: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Loginpage();
+                      }));
+                    })
+              ],
+            ),
           ),
         ));
   }
